2ec4c48feebbabfb47d67fd741c3dfade3d2a359
Mohit Joshi <abhishekdumka2244@gmail.com>
2025-08-07 20:22:02 +0530

    up

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -5,9 +5,13 @@ const {protect} = require('../middlewares/authMiddleware');
 const upload = require('../middlewares/uploadMiddleware');
 const router = express.Router();
 
+
+
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/profile', protect, getUserProfile);
+
+
 
 router.post("/upload-image", upload.single("image") , (req, res) => {
     if (!req.file) {
